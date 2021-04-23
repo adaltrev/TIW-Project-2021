3c4df1b2e1676fc8abff6f5d89009e34936b9cc6
Adalberto Trevisani <dummy.n45@hotmail.it>
2021-04-23 13:02:42 +0200

    SubmitSearch servlet, beginning of GetResults

--- Tiw_2021_Carrello/src/main/java/controllers/GetResults.java
+++ Tiw_2021_Carrello/src/main/java/controllers/GetResults.java
@@ -1,41 +1,50 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GoToResults
- */
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import utils.ConnectionHandler;
+
+
 @WebServlet("/GetResults")
 public class GetResults extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+	private TemplateEngine templateEngine;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public GetResults() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
 
 }
---
+++ Tiw_2021_Carrello/src/main/java/controllers/SubmitSearch.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+
+@WebServlet("/SubmitSearch")
+public class SubmitSearch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+    public SubmitSearch() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String search;
+		String path; 
+		search= StringEscapeUtils.escapeJava(request.getParameter("search"));
+		if(search==null || search.isEmpty()) {
+			path = getServletContext().getContextPath() + "/GoToHome";
+			response.sendRedirect(path);
+		}
+		
+		else {
+			path = getServletContext().getContextPath() + "/GetResults";
+			response.sendRedirect(path);
+		}
+	}
+
+}
