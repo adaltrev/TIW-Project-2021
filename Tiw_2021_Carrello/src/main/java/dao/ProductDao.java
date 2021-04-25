7e9b70d508276584a76a6d1b89fd8face6786126
Adalberto Trevisani <dummy.n45@hotmail.it>
2021-04-25 12:24:43 +0200

    Minor fixes

--- Tiw_2021_Carrello/src/main/java/dao/ProductDao.java
+++ Tiw_2021_Carrello/src/main/java/dao/ProductDao.java
@@ -70,12 +70,9 @@ public class ProductDao {
 		String query="SELECT p.id, p.name, s.price FROM product AS p JOIN sells AS s ON p.id=s.product_id WHERE p.name LIKE ? AND s.price IN (SELECT MIN(s2.price) FROM sells AS s2 WHERE s2.product_id=s.product_id) ORDER BY s.price ASC";
 		
 		//Find every product corresponding to search input
-
 		try(PreparedStatement pstatement = connection.prepareStatement(query);){
 			pstatement.setString(1, "%"+search+"%");
-			System.out.println("begin execute query");
 			try (ResultSet result = pstatement.executeQuery();) {
-				System.out.println("done");
 				if (!result.isBeforeFirst()) // If there are no results, it returns null
 					return null;
 				
@@ -84,7 +81,6 @@ public class ProductDao {
 					product.setId(result.getInt("id"));
 					product.setName(result.getString("name"));
 					map.put(product, result.getFloat("price"));
-					System.out.println(map.get(product));
 				}
 			}
 		}
