7c0df29d988dd1ae300cc69348d5a2396b1a3b5c
serra <emanuele.serraiotto@mail.polimi.it>
2021-04-30 12:01:22 +0200

    sistemato il caso in cui si inseriva in getDetails un id di un prodoto inisistente

--- Tiw_2021_Carrello/src/main/java/controllers/GetDetails.java
+++ Tiw_2021_Carrello/src/main/java/controllers/GetDetails.java
@@ -24,19 +24,17 @@ import dao.ProductDao;
 import dao.SellerDao;
 import utils.ConnectionHandler;
 
-
 @WebServlet("/GetProductDetails")
 public class GetDetails extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
 	private TemplateEngine templateEngine;
 
-       
-    public GetDetails() {
-        super();
-    }
+	public GetDetails() {
+		super();
+	}
 
-    public void init() throws ServletException {
+	public void init() throws ServletException {
 		connection = ConnectionHandler.getConnection(getServletContext());
 		ServletContext servletContext = getServletContext();
 		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
@@ -45,70 +43,62 @@ public class GetDetails extends HttpServlet {
 		this.templateEngine.setTemplateResolver(templateResolver);
 		templateResolver.setSuffix(".html");
 	}
-    
-    
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Integer id= Integer.parseInt(request.getParameter("id"));
-		ProductDao productDao= new ProductDao(connection);
-		SellerDao sellerDao= new SellerDao(connection);
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		ProductDao productDao = new ProductDao(connection);
+		SellerDao sellerDao = new SellerDao(connection);
 		Product product;
 		List<Seller> sellers;
-		
-		try {
-		product=productDao.findProductById(id);
-		}catch(SQLException e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-					"Server unavailable, not possible to show search results");
-			return;
-		}
-		if(product==null) {
-			response.sendError(HttpServletResponse.SC_NOT_FOUND,
-					"Page not found");
-		}
-		
-		try {
-		sellers=sellerDao.findSellersByProduct(product.getId());
-		}catch(SQLException e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-					"Server unavailable, not possible to show search results");
-			return;
-		}
-		
-		
-		ServletContext servletContext = getServletContext();
-		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-		String path="/WEB-INF/Details.html";
 
-		
-		//Cookie cookies[] = request.getCookies();
-		String cookieId=Integer.toString(product.getId());
-		/*if(cookies.length>=6) {
-			String name =cookies[1].getName();
-			Cookie removed=new Cookie(name,"");
-			removed.setMaxAge(0);
-			response.addCookie(removed);
-		}*/
-		Cookie ck=new Cookie("visualized_product_"+cookieId,"");
-		ck.setMaxAge(0);
-		response.addCookie(ck);
-		ck=new Cookie("visualized_product_"+cookieId,cookieId);
-		ck.setMaxAge(604800);
-		response.addCookie(ck);
-		
-		
-		if(sellers==null||sellers.size()==0)
-			ctx.setVariable("errorMsg", "No sellers for this product");
-		else {
-			ctx.setVariable("found", "Sellers for this product: ");
-			ctx.setVariable("rangeMsg","Price ranges for this seller: ");
+		try {
+			product = productDao.findProductById(id);
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+					"Server unavailable, not possible to show search results");
+			return;
 		}
-		ctx.setVariable("product", product);
-		ctx.setVariable("sellers", sellers);
-		templateEngine.process(path, ctx, response.getWriter());
-		
+		if (product == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND, "Page not found");
+		} else {
+
+			try {
+				sellers = sellerDao.findSellersByProduct(product.getId());
+			} catch (SQLException e) {
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+						"Server unavailable, not possible to show search results");
+				return;
+			}
+
+			ServletContext servletContext = getServletContext();
+			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+			String path = "/WEB-INF/Details.html";
+
+			// Cookie cookies[] = request.getCookies();
+			String cookieId = Integer.toString(product.getId());
+			/*
+			 * if(cookies.length>=6) { String name =cookies[1].getName(); Cookie removed=new
+			 * Cookie(name,""); removed.setMaxAge(0); response.addCookie(removed); }
+			 */
+			Cookie ck = new Cookie("visualized_product_" + cookieId, "");
+			ck.setMaxAge(0);
+			response.addCookie(ck);
+			ck = new Cookie("visualized_product_" + cookieId, cookieId);
+			ck.setMaxAge(604800);
+			response.addCookie(ck);
+
+			if (sellers == null || sellers.size() == 0)
+				ctx.setVariable("errorMsg", "No sellers for this product");
+			else {
+				ctx.setVariable("found", "Sellers for this product: ");
+				ctx.setVariable("rangeMsg", "Price ranges for this seller: ");
+			}
+			ctx.setVariable("product", product);
+			ctx.setVariable("sellers", sellers);
+			templateEngine.process(path, ctx, response.getWriter());
+
+		}
 	}
-
-
 
 }
